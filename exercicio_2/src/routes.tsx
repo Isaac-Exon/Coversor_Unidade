bf20d618a19b7cc53d526b57bce911d7b0786233
Isaac - Exon <isaac.souza@exongenomics.com>
2024-09-06 20:12:53 -0300

    ajustes do codigo

--- exercicio_2/src/routes.tsx
+++ exercicio_2/src/routes.tsx
@@ -1,19 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 
-import { AuthProvider } from "./context.tsx/AuthContext";
-import { UserProvider } from "./context.tsx/UserContext";
-
 import Conversor from "./pages/TelaInicial";
 
 function Rotas() {
   return (
-    <AuthProvider>
-      <UserProvider>
-        <Routes>
-          <Route path="/" element={<Conversor />} />
-        </Routes>
-      </UserProvider>
-    </AuthProvider>
+    <Routes>
+      <Route path="/" element={<Conversor />} />
+    </Routes>
   );
 }
 
